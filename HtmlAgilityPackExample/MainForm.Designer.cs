0d2c3bd6c524ceff4577c62e5b3333f3a659013a
Free Web Scraping Tool <nik@mydataprovider.com>
2015-12-30 17:37:07 +0300

    Amazon.com search item by name added

--- HtmlAgilityPackExample/MainForm.Designer.cs
+++ HtmlAgilityPackExample/MainForm.Designer.cs
@@ -28,12 +28,59 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.textBoxInput = new System.Windows.Forms.TextBox();
+            this.buttonSearch = new System.Windows.Forms.Button();
+            this.textBoxOutput = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textBoxInput
+            // 
+            this.textBoxInput.Location = new System.Drawing.Point(23, 28);
+            this.textBoxInput.Name = "textBoxInput";
+            this.textBoxInput.Size = new System.Drawing.Size(650, 20);
+            this.textBoxInput.TabIndex = 0;
+            this.textBoxInput.Text = "iphone 6s 32 gb";
+            // 
+            // buttonSearch
+            // 
+            this.buttonSearch.Location = new System.Drawing.Point(679, 28);
+            this.buttonSearch.Name = "buttonSearch";
+            this.buttonSearch.Size = new System.Drawing.Size(75, 23);
+            this.buttonSearch.TabIndex = 1;
+            this.buttonSearch.Text = "Search";
+            this.buttonSearch.UseVisualStyleBackColor = true;
+            this.buttonSearch.Click += new System.EventHandler(this.buttonSearch_Click);
+            // 
+            // textBoxOutput
+            // 
+            this.textBoxOutput.Location = new System.Drawing.Point(23, 71);
+            this.textBoxOutput.Multiline = true;
+            this.textBoxOutput.Name = "textBoxOutput";
+            this.textBoxOutput.Size = new System.Drawing.Size(731, 479);
+            this.textBoxOutput.TabIndex = 2;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(784, 562);
+            this.Controls.Add(this.textBoxOutput);
+            this.Controls.Add(this.buttonSearch);
+            this.Controls.Add(this.textBoxInput);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "MainForm";
+            this.Text = "Example";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox textBoxInput;
+        private System.Windows.Forms.Button buttonSearch;
+        private System.Windows.Forms.TextBox textBoxOutput;
     }
 }
 
--- HtmlAgilityPackExample/MainForm.cs
+++ HtmlAgilityPackExample/MainForm.cs
@@ -4,8 +4,13 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Windows.Forms;
+
+//Author: Nikolai Kekish
+//Web: http://mydataprovider.com
+//Email: sales@mydataprovider.com
 
 namespace HtmlAgilityPackExample
 {
@@ -14,6 +19,30 @@ namespace HtmlAgilityPackExample
         public MainForm()
         {
             InitializeComponent();
+        }
+
+        private void buttonSearch_Click(object sender, EventArgs e)
+        {
+            //http://www.amazon.com/s/ref=nb_sb_noss_2?url=search-alias%3Daps&field-keywords=iphone+6s
+            try
+            {
+                textBoxOutput.Text = "";
+                var url = "http://www.amazon.com/s/ref=nb_sb_noss_2?url=search-alias%3Daps&field-keywords=" + System.Web.HttpUtility.UrlEncode(textBoxInput.Text);
+                var client = new WebClient();
+                var html = client.DownloadString(url);
+
+                var doc = new HtmlAgilityPack.HtmlDocument();
+                doc.LoadHtml(html);
+                var a = doc.DocumentNode.SelectSingleNode("//div[@class=\"s-item-container\"]//a");
+                if (a != null)
+                {
+                    textBoxOutput.Text = a.Attributes["href"].Value.ToString();
+                }
+            }
+            catch (Exception ex)
+            { 
+            }
+
         }
     }
 }
